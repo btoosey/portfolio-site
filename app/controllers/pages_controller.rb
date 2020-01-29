036be61337ce2497316473c40cbae996b457eb05
Ben Toosey <benjamin.toosey@gmail.com>
2020-01-29 19:50:36 +0000

    FIX: updated meta tags with gem meta_tags

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
+    set_meta_tags og: {
+      title: "Benjamin Toosey | Portfolio",
+      description: "Welcome to my personal portfolio website! Take a look at what I can do and previous projects I have worked on.",
+      type: 'website',
+      image: '../../assets/images/hellothere.png'
+    }
+
     @web_projects = WebProject.all
   end
 end
